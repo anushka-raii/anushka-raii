cb8972a813381e371e06d30e5583c060d754dccf
anushka <154774980+anushka-raii@users.noreply.github.com>
2025-03-02 21:37:51 +0530

    rectangle

---
+++ AreaOfRectangle.java
@@ -0,0 +1,13 @@
+public class AreaOfRectangle 
+{ 
+    public static void main(String[] args)
+ { 
+        double length = 10.0; 
+        double width = 5.0; 
+         
+        double area = length * width;
+
+  System.out.println("The area of the rectangle is: " + area); 
+
+    } 
+}
