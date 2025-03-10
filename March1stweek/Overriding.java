e2321e793ef55e2ae8e76ca2942e0f040c88223b
anushka <154774980+anushka-raii@users.noreply.github.com>
2025-03-10 16:44:45 +0530

    overriding

---
+++ March1stweek/Overriding.java
@@ -0,0 +1,21 @@
+class Father {
+    void drive() {
+        System.out.println("Driving the car slowly...");
+    }
+}
+
+class Son extends Father {
+    @Override
+    void drive() {
+        System.out.println("Driving the car very fast...");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Father f = new Father();
+        f.drive(); 
+        
+        Son s = new Son();
+        s.drive(); 
+}
